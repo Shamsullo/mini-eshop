fd566d6c663e6dc7aa6decb611ad5221c10b9331
Shamsullo <shamsulloismatov@gmail.com>
2021-03-02 18:11:22 +0600

    database setting changes -hostname

--- mini_shop_v1/mini_shop/settings.py
+++ mini_shop_v1/mini_shop/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'mini_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbfrcg93g8kaj',
+        'NAME': 'ec2-18-211-97-89.compute-1.amazonaws.com',
         'USER': 'xonjrkunkpxsyg',
         'PASSWORD': '9f55a99206d66fca56f54d16ecbdda6c63557e03250f6eadced3a4c505e7ef57',
         'HOST': 'postgres://xonjrkunkpxsyg:9f55a99206d66fca56f54d16ecbdda6c63557e03250f6eadced3a4c505e7ef57@ec2-18-211-97-89.compute-1.amazonaws.com:5432/dbfrcg93g8kaj',
